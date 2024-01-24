0b25125ae3dd465c75cf670c8b2b9fe2c4c884d9
caitlinmag <caitlinmaguire14@outlook.com>
2024-01-24 19:24:18 +0000

    Working on Question 2
    Wrote comments to help my understanding of what the question is asking. I created a stack which takes the users row & column for a starting point. Made a while loop for while the stack is not empty, and what will be carried out when the while loop is true.

--- oop-ca3-caitlin-maguire/src/CA3_Question2.java
+++ oop-ca3-caitlin-maguire/src/CA3_Question2.java
@@ -6,6 +6,29 @@ import java.util.Stack;
  * Class Group:
  */
 public class CA3_Question2 {
+    /*
+    - Empty pixels will be given colour
+    - Stop when reach occupied pixel
+    - Get the starting row & column - starting cell
+    - push (row,column)pair on to stack
+    - Make simple pair class storing the row & column DONE
+
+    - While loop for if stack is not empty
+    do these until stack is empty :
+    - POP off (row,column)pair from top of stack -
+    if the element is the top then pop it off
+    - not filled then - fill the corresponding cell with a number
+    the number is added on at each step to show order of fill - maybe use a count
+    - PUSH coordinates(row,column)pair of unfilled neighbours(still 0) in north,east,south & west directions on the stack
+     north - one row up
+     east - one column to right
+     south - one row down
+     west - one column to left
+     -> idea is to keep checking if neighbours are 0 until there is no values of 0 in the 2d array
+
+    - stack is empty then print the entire 2D array
+     */
+
     //Simple pair class - to store row and column
     public static class pair {
         private int c;
@@ -32,9 +55,10 @@ public class CA3_Question2 {
             this.r = r;
         }
 
-        Stack<Integer> fillStack = new Stack<Integer>();
-//        fillStack.push(r,c);
     }
+
+    //stack
+    private static Stack<Integer> fillStack = new Stack<Integer>();
 
     /*
         Starter function to create the 2D array and populate it with 0
@@ -69,6 +93,7 @@ public class CA3_Question2 {
         Scanner kb = new Scanner(System.in);
         int userRow;
         int userColumn;
+        int fill = 1;
 
         System.out.println("Enter starting row:");
         userRow = kb.nextInt();
@@ -76,10 +101,35 @@ public class CA3_Question2 {
         System.out.println("Enter starting column:");
         userColumn = kb.nextInt();
 
+        fillStack.push(userRow);
+        fillStack.push(userColumn);
+        System.out.println(fillStack);
+
+        //keep track of what cell it is on (row,column)
+        int currentCell;
+        //While the stack is not empty
+        while (!fillStack.isEmpty()) {
+            //check top element
+//            if (fillStack.peek() == currentCell) {
+                //pop off (r,c) from top
+//                fillStack.pop(currentCell);
+//            }
+
+            //use the getters & setters to get the rows & columns
+
+            //Check if rows & columns are 0 and then increment them to now have the next fill number
+            if (r == 0 || c == 0) {
+                //increment the fill value
+                fill++;
+            }
+//            fillStack.push(currentCell);
+        }
+
     }
 
     public static void start() {
         int[][] arr = floodFillStart();
+        fill(0, 0, arr);
         display(arr);
     }
 
