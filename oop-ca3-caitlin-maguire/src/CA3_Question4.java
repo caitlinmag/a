a86218c1ed031c298ec14af6bb624db7123ae614
caitlinmag <caitlinmaguire14@outlook.com>
2024-01-28 16:19:23 +0000

    Working on question 3
    Having issues with reading from the file, I attempted the code to use a scanner with delimiter, and created a hashmap and a while loop to scan through the file putting the identifer and its line number in the map.

--- oop-ca3-caitlin-maguire/src/CA3_Question4.java
+++ oop-ca3-caitlin-maguire/src/CA3_Question4.java
@@ -2,17 +2,30 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Stack;
+
 /**
- *  Name:
- *  Class Group:
+ * Name:
+ * Class Group:
  */
 public class CA3_Question4 {
+/*
+Using stack
+-in the powerpoint - 15.6 stack and queue applications -
+slide 55 is similar to the question on html tags-
+only difference is dealing with the opening and closing tags
+
+Program to check if a sequence of HTML tags are properly nested
+For each opening tag (eg <p> must have a closing tag </p>)
+- p tag may have other tags (eg <p> <ul> <li> </li> </u> <a> </p>)
+inner tags must be closed before outer tags
+process a file containing tags
+ASSUME tags are separated by spaces & there is no text inside tags
+ */
 
     /*
         filename: name of the file to test.
      */
-    public static boolean validate(String filename) throws FileNotFoundException
-    {
+    public static boolean validate(String filename) throws FileNotFoundException {
         return false;
     }
 
@@ -26,13 +39,42 @@ public class CA3_Question4 {
      */
     public static void main(String[] args) throws FileNotFoundException {
         String[] files = {"tags_valid.txt", "tags_invalid.txt"};
-        for(String fName: files) {
-            System.out.print(fName +": ");
+        for (String fName : files) {
+            System.out.print(fName + ": ");
             if (validate(fName)) {
                 System.out.println("This file is valid");
             } else {
                 System.out.println("This file is invalid");
             }
+        }
+
+        //Create stack to hold html tags
+        Stack<String> htmlStack = new Stack<String>();
+
+//        File f = new File(fileName);
+        //scanner to scan through file
+//        Scanner in = new Scanner();
+
+        String tag = "";
+
+        //opening tag - push to stack
+        htmlStack.push(tag);
+
+        //if opening and closing tags dont match
+        //stack is not empty
+        while (!htmlStack.isEmpty()) {
+            htmlStack.pop();
+
+            //if at the end of the stack is empty (top element)
+            if (!htmlStack.peek().isEmpty()) {
+
+            } else {
+                //not balanced
+                //closing tag - pop the stack
+                htmlStack.pop();
+            }
+
+
         }
     }
 }
