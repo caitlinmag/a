6348ea80f60c98b3ce210ace1d1e142272d80f9c
caitlinmag <caitlinmaguire14@outlook.com>
2024-01-29 16:07:18 +0000

    All classes were inside another folder.

---
+++ src/CA3_Question2.java
@@ -0,0 +1,161 @@
+import java.security.KeyPair;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Name:
+ * Class Group:
+ */
+public class CA3_Question2 {
+    /*
+    - Empty pixels will be given colour
+    - Stop when reach occupied pixel
+    - Get the starting row & column - starting cell
+    - push (row,column)pair on to stack
+    - Make simple pair class storing the row & column DONE
+
+    - While loop for if stack is not empty
+    do these until stack is empty :
+    - POP off (row,column)pair from top of stack -
+    if the element is the top then pop it off
+    - not filled then - fill the corresponding cell with a number
+    the number is added on at each step to show order of fill - maybe use a count
+    - PUSH coordinates(row,column)pair of unfilled neighbours(still 0) in north,east,south & west directions on the stack
+     north - one row up
+     east - one column to right
+     south - one row down
+     west - one column to left
+     -> idea is to keep checking if neighbours are 0 until there is no values of 0 in the 2d array
+
+    - stack is empty then print the entire 2D array
+     */
+
+    //Simple pair class - to store row and column
+    public static class pair {
+        private int r;
+        private int c;
+
+        public int getR() {
+            return r;
+        }
+
+        public int getC() {
+            return c;
+        }
+
+        public void setR(int r) {
+            this.r = r;
+        }
+
+        public void setC(int c) {
+            this.c = c;
+        }
+
+        public pair(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    /*
+        Starter function to create the 2D array and populate it with 0
+     */
+    public static int[][] floodFillStart() {
+
+        int[][] arr = new int[10][10];
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
+                arr[x][y] = 0;
+            }
+        }
+        return arr;
+    }
+    /*
+        Helper function to display the image
+        10 x 10 array
+     */
+
+    public static void display(int[][] arr) {
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
+                System.out.printf("%4d", arr[x][y]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void fill(int r, int c, int[][] arr) {
+        //fillOrder used to keep track of coordinates, incrementing the number for each coordinate
+        int fillOrder = 0;
+
+        //create stack to hold row and column pairs
+        Stack<pair> fillStack = new Stack<pair>();
+
+        //push the row & column to the stack as a pair
+        fillStack.push(new pair(r, c));
+
+        //if I make a pair object
+        //use this object to get the peek top element
+        //make int variables for i and j, make i and j equal to currentCell.getR(); and currentCell.getC();
+        //then change r and c in for loop to i and j
+        //try keep r and c in the for loop bracket, and change the rest in the if statements to i and j
+
+        //keep track of what cell it is on (row,column)
+        //While the stack is not empty
+        while (!fillStack.isEmpty()) {
+            // pop off (r,c) from top
+            fillStack.pop();
+
+            //iterate through the array, checking the rows and columns
+            for (r = 0; r < arr.length; r++) {
+                for (c = 0; c < arr.length; c++) {
+                    //check if the coordinates are 0
+                    if (arr[r][c] == 0) {
+                        //increment fill for each coordinate
+                        arr[r][c] = fillOrder++;
+
+                        //check neighbour coordinates and fill to the next fillOrder number
+                        //north - one row up
+                        if (r - 1 > 0 && arr[r - 1][c] == 0) {
+                            //push coordinates on to stack
+                            fillStack.push(new pair(r - 1, c));
+                        }
+                        //east - one column to right
+                        //max is 10
+                        if (c + 1 < 10 && arr[r][c + 1] == 0) {
+                            fillStack.push(new pair(r, c + 1));
+                        }
+                        //south - one row down
+                        if (r + 1 < 10 && arr[r + 1][c] == 0) {
+                            fillStack.push(new pair(r + 1, c));
+                        }
+                        //west - one column to the left
+                        if (c - 1 > 0 && arr[r][c - 1] == 0) {
+                            fillStack.push(new pair(r, c - 1));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void start() {
+        //Prompt for starting row and column - starting cell for the flood fill
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("Enter starting row:");
+        int r = kb.nextInt();
+
+        System.out.println("Enter starting column:");
+        int c = kb.nextInt();
+
+        int[][] arr = floodFillStart();
+        fill(r, c, arr);
+        display(arr);
+    }
+
+    public static void main(String[] args) {
+        start();
+    }
+
+}
---
+++ src/CA3_Question3.java
@@ -0,0 +1,70 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * Name:
+ * Class Group:
+ */
+
+public class CA3_Question3 {
+    /*
+       Reading file
+       produce index of all identifiers in file
+       identifiers -> variable names, class names , and keywords
+       Declare scanner in for reading file   DONE
+       call in.useDelimiter("[^A-Za-z0-9_]+") DONE
+       Each identifier print all lines, with line number
+       consider identifier as string consisting only of letters, numbers , and underscrores DONE
+       each call to next returns identifier DONE
+     */
+
+    public static void readFile(String fileName) throws FileNotFoundException {
+        //read a java source file - fileName defined in the main
+        File f = new File(fileName);
+
+        //Declare scanner in to read from the source file
+        Scanner in = new Scanner(f);
+
+        //call delimiter - identifier considered as a string consisting only of letters, numbers , and underscrores
+        in.useDelimiter("[^A-Za-z0-9_]+");
+
+        //create a map to hold identifiers(String) and their line numbers(Integer)
+        Map<String, Integer> identifierMap = new HashMap<>();
+
+        //key is the identifier
+        String key = "";
+        //value is the line number
+        int value = 0;
+
+        //count line numbers starting from 0
+        int lineNum = 0;
+
+        //create a while loop to scan through the file
+        while (in.hasNext()) {
+            //calling in.next() returns an identifier
+            key = in.next();
+            //increment the line number for each identifier
+            lineNum++;
+            //Put the identifier and its line number into the hash map
+            identifierMap.put(key, lineNum);
+            System.out.println(key + lineNum);
+        }
+
+//        Set<String> keyset = identifierMap.keySet();
+//        //display identifiers and the corresponding line numbers
+//        System.out.println("All identifiers and corresponding line numbers");
+//       //enhanced for loop to iterate through identifiers and print identifier and line number
+//        for (String identifiers: keyset){
+//            System.out.println(identifiers + " -> " + identifierMap.get(identifiers));
+//        }
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        readFile("src/CA3_Question1.java");
+    }
+}
