8b7be038052520af87285537fbf5efa1a26fe022
caitlinmag <caitlinmaguire14@outlook.com>
2024-02-07 19:31:42 +0000

    Started question 8

--- src/CA3_Question8.java
+++ src/CA3_Question8.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
+import java.util.Stack;
+
 /**
- *  Name:
- *  Class Group:
+ * Name:
+ * Class Group:
  */
 public class CA3_Question8 {
+    /*
+    Implement a calculator to evaluate arithmetic expressions for the operators + - * / and
+    parenthesis ( ). See the accompanying PDF document which is an extract from a book
+    explaining the algorithm.
+
+    Using a stack
+     */
+
 
     /*
         Reads in an equation from the user
@@ -13,5 +23,38 @@ public class CA3_Question8 {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter equation");
         equation = in.nextLine().trim();
+
+        Stack<Integer> results = new Stack<Integer>();
+        boolean finished = false;
+
+        while (!finished) {
+            if (in.equals("+")) {
+                results.push(results.pop() + results.pop());
+            } else if (in.equals("-")) {
+                Integer arg2 = results.pop();
+                results.push(results.pop() - arg2);
+            } else if (in.equals("*") || in.equals("x")) {
+                results.push(results.pop() * results.pop());
+            } else if (in.equals("/")) {
+                Integer arg2 = results.pop();
+                results.push(results.pop() / arg2);
+            } else if (in.equals("()")) {
+
+            } else if (in.equals("q") || in.equals("Q")) {
+                finished = true;
+            }else{
+                //not operator - integer
+//                results.push(Integer.parseInt(in));
+            }
+            System.out.println(results);
+        }
+
+        //read number - push to stack
+
+        //read operand - pop TWO values off stack
+        //combine values with operand - push result on to stack
+
+        //No more input - pop and display result
+
     }
 }
